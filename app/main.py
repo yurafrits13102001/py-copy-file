86b431d0da7a397a4bd270f3ed8009d87b003770
yuraf <twinkreferal22847228@gmail.com>
2025-08-13 16:31:41 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ import os.path
 def copy_file(files_names: str) -> None:
     file_list = files_names.split(" ")
     if len(file_list) != 3 or file_list[0] != "cp":
-        return None
+        return
 
     _, file1, file2 = file_list
 
     if file1 == file2:
-        return None
+        return
 
     if not os.path.exists(file1):
-        return None
+        return
 
     with open(file1, "rb") as file_1, open(file2, "wb") as file_2:
         data = file_1.read()
